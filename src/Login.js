cf2ca5996a9dae4c0bb513226807fccbd2d4601d
Jisoon <siriusda2@naver.com>
2020-12-26 00:02:00 +0900

    2020/12/26

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from './firebase/firebase';
 import styled from 'styled-components/macro';
 
 function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [name, setName] = useState("");
+
+	const loginToApp = (e) => {
+		e.preventDefault();
+	}
+
+	const register = () => {
+	}
+	
 	return (
 		<LoginContainer>
 			<img 
-				src="https://www.brandeps.com/logo-download/L/Linkedin-logo-vector-01.svg"
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/LinkedIn_Logo_2013.svg/268px-LinkedIn_Logo_2013.svg.png"
 				alt=""
 			/>
 			<form>
-				<input placeholder="Full name (required if registering" type="text" />
+				<input placeholder="Full name (required if registering)" type="text" />
+				<input placeholder="Profile pic URL (optional)" type="text" />
+				<input 
+					value={email} 
+					onChange={e => setEmail(e.target.value)} 
+					placeholder="Email" 
+					type="email" 
+				/>
+				<input 
+					value={password} 
+					onChange={e => setPassword(e.target.value)} 
+					placeholder="Password" 
+					type="password" 
+				/>
+				<button type="submit" onClick={loginToApp}>Sign in</button>
 			</form>
+			<p>Not a member?{" "}
+				<LoginRegister onClick={register}>Register Now</LoginRegister>
+			</p>
 		</LoginContainer>
 	)
 }
@@ -24,4 +53,40 @@ const LoginContainer = styled.div`
 	margin-right: auto;
 	padding-top: 100px;
 	padding-bottom: 100px;
+
+	& img {
+		object-fit: contain;
+		height: 70px;
+		margin-top: 20px;
+		margin-bottom: 20px;
+	}
+
+	& form {
+		display: flex;
+		flex-direction: column;
+		& input {
+			width: 350px;
+			height: 50px;
+			font-size: 20px;
+			padding-left: 10px;
+			margin-bottom: 10px;
+			border-radius: 5px;
+		}
+		& button {
+			width: 365px;
+			height: 50px;
+			font-size: large;
+			color: #fff;
+			background-color: #0074b1;
+			border-radius: 5px;
+		}
+	}
+	p {
+		margin-top: 20px;
+	}
+`;
+
+const LoginRegister = styled.span`
+	color: #0177b7;
+	cursor: pointer;
 `;
